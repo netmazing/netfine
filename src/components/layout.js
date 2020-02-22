8e4a179f7d16d0bcd10f1c72a5f1536b5cc79a9b
Dev <p.trojnacki@o2.pl>
2020-02-22 18:57:30 +0100

    commit 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import Cursor from './Cursor';
 // import '../css/layout.css';
 import './logo.css';
 import '../scss/layout.scss';

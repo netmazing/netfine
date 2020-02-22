250cee180ff956bce43281bd172662beded34299
Dev <p.trojnacki@o2.pl>
2020-02-22 19:38:59 +0100

    ok6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import Loader from '../components/Loader';
 import {Parallax} from 'react-parallax';
 import Fade from 'react-reveal/Fade';

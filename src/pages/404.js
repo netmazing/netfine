45ae3c447fbc6ba127c25038b5b45869691494fe
Pawel Trojnacki <p.trojnacki@o2.pl>
2020-03-26 17:17:26 +0100

    create error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,30 @@
 import React from "react";
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import '../scss/error-page.scss';
+import SEO from '../components/SEO';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import ArrowRight from '../svg/arrow-right.svg';
+
 
 const NotFoundPage = () => (
   <Layout>
-    <div>error page</div>
+    <SEO siteTitle="404 :("/>
+    <main className="page error-page">
+        <div className="error-page__wrapper">
+          <h1 className="title error-title">Ooops, chyba zabłądziłeś</h1>
+          <p className-="text">Nie ma takiej strony</p>
+          <AniLink
+          cover
+          to="/oferta"
+          direction="left"
+          duration="1.5"
+          bg='#000'
+          className="btn">
+            Strona główna
+           <img className="arrow-right" src={ArrowRight} alt="netfine.pl"/>
+          </AniLink>
+        </div>
+      </main>
   </Layout>
   
 )

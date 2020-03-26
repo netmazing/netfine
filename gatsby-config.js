ce0e6336c0a2264670bbc9401c7ac25776c0026b
Pawel Trojnacki <p.trojnacki@o2.pl>
2020-03-26 21:33:20 +0100

    update manifest.json and add gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,17 +51,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Netfine`,
+        short_name: `Netfine`,
         start_url: `/`,
-        background_color: `#ffffff`,
+        background_color: `#fffff`,
         theme_color: `#ffffff`,
-        display: `minimal-ui`,
+        display: `fullscreen`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-offline'
   ],
 }
